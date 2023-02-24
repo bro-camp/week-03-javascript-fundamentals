a698301c4cab54095df2b714de4a43929382dc91
I-_-I <>
2023-02-24 15:59:37 +0530

    q-15.js

--- src/q-15.js
+++ src/q-15.js
@@ -3,29 +3,21 @@ const readlineSync = require('readline-sync');
 function getArray(size) {
   const array = [];
   for (let i = 0; i < size; i += 1) {
-    const ar = [];
-    for (let j = 0; j < size; j += 1) {
-      ar.push(parseInt(readlineSync.question(''), 10));
-    }
-    array.push(ar);
+    array.push(parseInt(readlineSync.question(''), 10));
   }
   return array;
 }
 
 function displayArray(array) {
   const size = array.length;
-  console.log('[');
+  let line = '[ ';
   for (let i = 0; i < size; i += 1) {
-    let line = '  [ ';
-    for (let j = 0; j < size; j += 1) {
-      line += array[i][j];
+    line += array[i];
 
-      if (j !== size - 1) line += ', ';
-      else line += ' ],';
-    }
-    console.log(line);
+    if (i !== size - 1) line += ', ';
+    else line += ' ]\n\n';
   }
-  console.log(']\n\n');
+  console.log(line);
 }
 
 function main() {
